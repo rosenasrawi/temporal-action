cbf1dc4c08c0f850a335eeda794ed6eb3268476e
Rose Nasrawi <r.nasrawi11@gmail.com>
2023-01-16 15:59:12 +0100

    Separate task functions from main script

--- functions.py
+++ functions.py
@@ -159,26 +159,3 @@ def showFeedback(perf):
         fixcross.draw(); feedback.draw()
         window.flip()
 
-def runBlock(block, trials):
-
-    showCue(block)
-
-    for _ in range(trials):
-
-        tori = showStim()
-        if block == 'Second': tori.reverse()
-
-        perf = 0
-
-        for i, targori in enumerate(tori):
-
-            key, turns = showDial(); 
-            if i == 0: showFix(timing['del3'])
-
-            repori = getReportori(key, turns)
-            perf += getPerformance(repori, targori)
-
-        showFeedback(str(round(perf/2)))
-        
-runBlock(block = 'First', trials = 5)
-runBlock(block = 'Second', trials = 5)
